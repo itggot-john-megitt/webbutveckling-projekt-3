18160fda92b39ebb0b594b9f359a4a680a5ecc7a
itggot-john-megitt <john.megitt@elev.ga.ntig.se>
2019-03-21 00:07:06 +0100

    added openMenu functionality, backgroundimage for the first element in main "ad"

---
+++ site/main.js
@@ -0,0 +1,9 @@
+function openMenu(event) {
+    document.getElementById("theMenu").style.width = "100vw";
+    document.getElementById("theMenu").style.opacity = "1";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav(event) {
+    document.getElementById("theMenu").style.width = "0";
+  }
