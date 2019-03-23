b46f1f96c2864e58ccabacb74e6566098887e207
itggot-john-megitt <john.megitt@elev.ga.ntig.se>
2019-03-23 20:28:38 +0100

    fel

--- site/main.js
+++ site/main.js
@@ -1,4 +1,16 @@
+<<<<<<< HEAD
 function openCloseMenu(event) {
   document.getElementById("theMenu").classList.toggle("menu-active")
   document.body.classList.toggle("noscroll")
 }
+=======
+function openMenu(event) {
+    document.getElementById("theMenu").style.width = "100vw";
+    document.getElementById("theMenu").style.opacity = "1";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav(event) {
+    document.getElementById("theMenu").style.width = "0";
+  }
+>>>>>>> parent of b37b243... added products and menu animation
