4825dcf26590a224715fbd650bd7c91254f29a21
itggot-john-megitt <john.megitt@elev.ga.ntig.se>
2019-03-24 02:25:57 +0100

    changed lots of thing with the products

--- site/main.js
+++ site/main.js
@@ -1,30 +1,36 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-function openCloseMenu(event) {
+function openCloseMenu(event){
   document.getElementById("theMenu").classList.toggle("menu-active")
   document.body.classList.toggle("noscroll")
 }
-=======
-=======
->>>>>>> parent of b37b243... added products and menu animation
-=======
->>>>>>> parent of b37b243... added products and menu animation
-function openMenu(event) {
-    document.getElementById("theMenu").style.width = "100vw";
-    document.getElementById("theMenu").style.opacity = "1";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav(event) {
-    document.getElementById("theMenu").style.width = "0";
-<<<<<<< HEAD
-<<<<<<< HEAD
-  }
->>>>>>> parent of b37b243... added products and menu animation
-=======
-  }
->>>>>>> parent of b37b243... added products and menu animation
-=======
-  }
->>>>>>> parent of b37b243... added products and menu animation
+function open1(){
+  document.getElementById("theInfo1").classList.toggle("info-active")
+  document.getElementById("theShadow").classList.toggle("shadow-active")
+}
+function open2(event){
+  document.getElementById("theInfo2").classList.toggle("info-active")
+  document.getElementById("theShadow").classList.toggle("shadow-active")
+}
+function open3(event){
+  document.getElementById("theInfo3").classList.toggle("info-active")
+  document.getElementById("theShadow").classList.toggle("shadow-active")
+}
+function open4(event){
+  document.getElementById("theInfo4").classList.toggle("info-active")
+  document.getElementById("theShadow").classList.toggle("shadow-active")
+}
+function open5(event){
+  document.getElementById("theInfo5").classList.toggle("info-active")
+  document.getElementById("theShadow").classList.toggle("shadow-active")
+}
+function open6(event){
+  document.getElementById("theInfo6").classList.toggle("info-active")
+  document.getElementById("theShadow").classList.toggle("shadow-active")
+}
+function open7(event){
+  document.getElementById("theInfo7").classList.toggle("info-active")
+  document.getElementById("theShadow").classList.toggle("shadow-active")
+}
+function open8(event){
+  document.getElementById("theInfo8").classList.toggle("info-active")
+  document.getElementById("theShadow").classList.toggle("shadow-active")
+}
