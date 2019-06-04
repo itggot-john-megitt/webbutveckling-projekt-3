6b4feb660dab589457e294a65dd139460649f07e
itggot-john-megitt <john.megitt@elev.ga.ntig.se>
2019-06-04 23:03:37 +0200

    Image size optimisation & popups can be closed by clicking outside them
    Skalade ner bilderna till rimliga storlekar för mobil och gjorde så att dessa används i mobilversionen av sidan. Lade till funktionaliteten av att kunna trycka utanför pop-up fönster för att stänga dem, gjorde detta genom att låta varje pop-up ha en tillhörande skugga som man kan trycka på. Jag vet att man borde kunna ha en funktion som öppnar och stänger alla olika måltids-pop-ups beroende på vilken måltid man först klickat på, men jag vet inte hur. Då hade man också kunnat undvika en egen skugga för varje pop-up.
    
    Förrförra commiten jag skulle göra råkade jag göra till slutprojektet, så förändringarna kom med i förra commiten i detta projekt, den var så här: Titel: "Divitis mostly cured" Beskrivning: "Majoriteten av divar har tagits bort och många onödiga id:n har bytits ut mot divar". Alltså finns få divar kvar och särskilt efter förra commiten inte så många id:n

--- site/main.js
+++ site/main.js
@@ -1,40 +1,40 @@
-function openCloseMenu(event){
+function openCloseMenu(){
   document.getElementById("theMenu").classList.toggle("menu-active")
   document.body.classList.toggle("noscroll")
 }
 function open1(){
   document.getElementById("theInfo1").classList.toggle("info-active")
-  document.getElementById("theShadow").classList.toggle("shadow-active")
+  document.getElementById("theShadow1").classList.toggle("shadow-active")
 }
-function open2(event){
+function open2(){
   document.getElementById("theInfo2").classList.toggle("info-active")
-  document.getElementById("theShadow").classList.toggle("shadow-active")
+  document.getElementById("theShadow2").classList.toggle("shadow-active")
 }
-function open3(event){
+function open3(){
   document.getElementById("theInfo3").classList.toggle("info-active")
-  document.getElementById("theShadow").classList.toggle("shadow-active")
+  document.getElementById("theShadow3").classList.toggle("shadow-active")
 }
-function open4(event){
+function open4(){
   document.getElementById("theInfo4").classList.toggle("info-active")
-  document.getElementById("theShadow").classList.toggle("shadow-active")
+  document.getElementById("theShadow4").classList.toggle("shadow-active")
 }
-function open5(event){
+function open5(){
   document.getElementById("theInfo5").classList.toggle("info-active")
-  document.getElementById("theShadow").classList.toggle("shadow-active")
+  document.getElementById("theShadow5").classList.toggle("shadow-active")
 }
-function open6(event){
+function open6(){
   document.getElementById("theInfo6").classList.toggle("info-active")
-  document.getElementById("theShadow").classList.toggle("shadow-active")
+  document.getElementById("theShadow6").classList.toggle("shadow-active")
 }
-function open7(event){
+function open7(){
   document.getElementById("theInfo7").classList.toggle("info-active")
-  document.getElementById("theShadow").classList.toggle("shadow-active")
+  document.getElementById("theShadow7").classList.toggle("shadow-active")
 }
-function open8(event){
+function open8(){
   document.getElementById("theInfo8").classList.toggle("info-active")
-  document.getElementById("theShadow").classList.toggle("shadow-active")
+  document.getElementById("theShadow8").classList.toggle("shadow-active")
 }
-function openCloseCart(event){
+function openCloseCart(){
   document.getElementById("theCart").classList.toggle("cart-active")
-  document.getElementById("theShadow").classList.toggle("shadow-active")
+  document.getElementById("theCartShadow").classList.toggle("shadow-active")
 }
