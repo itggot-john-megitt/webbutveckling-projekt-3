0313515740110d773bf7e57241f6aeafc7e5bc43
itggot-john-megitt <john.megitt@elev.ga.ntig.se>
2019-06-05 23:49:04 +0200

    Made javascript prettier and reduced number of shadows
    Förut fungerade inte det jag ville med att skapa en enda funktion för att öppna måltidsinfos. Jag försökte då döpa funktionen till open, vilket visade sig vara en inbyggd funktion som försökte öppna en ny url när man använde den (tror jag). Det gick att lösa så nu blev det lite mer kompakt.

--- site/main.js
+++ site/main.js
@@ -2,37 +2,15 @@ function openCloseMenu(){
   document.getElementById("theMenu").classList.toggle("menu-active")
   document.body.classList.toggle("noscroll")
 }
-function open1(){
-  document.getElementById("theInfo1").classList.toggle("info-active")
-  document.getElementById("theShadow1").classList.toggle("shadow-active")
+function showInfo(number){
+  document.getElementById("theInfo" + number).classList.toggle("info-active")
+  document.getElementById("theShadow").classList.toggle("shadow-active")
 }
-function open2(){
-  document.getElementById("theInfo2").classList.toggle("info-active")
-  document.getElementById("theShadow2").classList.toggle("shadow-active")
-}
-function open3(){
-  document.getElementById("theInfo3").classList.toggle("info-active")
-  document.getElementById("theShadow3").classList.toggle("shadow-active")
-}
-function open4(){
-  document.getElementById("theInfo4").classList.toggle("info-active")
-  document.getElementById("theShadow4").classList.toggle("shadow-active")
-}
-function open5(){
-  document.getElementById("theInfo5").classList.toggle("info-active")
-  document.getElementById("theShadow5").classList.toggle("shadow-active")
-}
-function open6(){
-  document.getElementById("theInfo6").classList.toggle("info-active")
-  document.getElementById("theShadow6").classList.toggle("shadow-active")
-}
-function open7(){
-  document.getElementById("theInfo7").classList.toggle("info-active")
-  document.getElementById("theShadow7").classList.toggle("shadow-active")
-}
-function open8(){
-  document.getElementById("theInfo8").classList.toggle("info-active")
-  document.getElementById("theShadow8").classList.toggle("shadow-active")
+function hideInfo() {
+  for (let e of document.getElementsByClassName("info-active")) {
+    e.classList.remove("info-active")
+  }
+  document.getElementById("theShadow").classList.remove("shadow-active")
 }
 function openCloseCart(){
   document.getElementById("theCart").classList.toggle("cart-active")
